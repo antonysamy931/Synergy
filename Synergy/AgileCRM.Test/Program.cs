77e7967a5ef5632a2d851c66cfc3d19bc7443290
Antony Samy Joseph <antonysamy931@gmail.com>
2017-03-02 23:37:27 +0530

    agilecrm added

---
+++ Synergy/AgileCRM.Test/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgileCRM.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /***************************************************************/
+            string result = string.Empty;
+            const string url = "https://antonysamy.agilecrm.com/dev/api/contacts";
+            String encoded = System.Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes("antonysamy931@gmail.com:kesfef51bnhrdqaud0l3siecbv"));            
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            request.Accept = "application/json";
+
+            request.Headers.Add("Authorization", "Basic " + encoded);
+            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            {
+                Stream dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                result = reader.ReadToEnd();
+
+                reader.Close();
+                dataStream.Close();
+                response.Close();
+            }
+        }
+    }
+}
